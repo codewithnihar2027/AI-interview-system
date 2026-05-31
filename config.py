ad999104459f055677d81ebbddb8c25aad9d7060
meghnarao06 <meghnarao06@gmail.com>
2026-05-31 15:17:55 +0530

    Removed API key from config

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
-GEMINI_API_KEY = "AQ.Ab8RN6K06oXQv77HA3jz_wKs4qRUD3SKUOkjqygn5OstO626Ag"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("AQ.Ab8RN6K06oXQv77HA3jz_wKs4qRUD3SKUOkjqygn5OstO626Ag")
